e2c94cf24af5a08e49b75dc94835f2a853527dbd
ToHuuDang <tohuudang14@gmail.com>
2023-02-27 21:36:29 +0700

    first commit

--- bai1.cpp
+++ bai1.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
-
 using namespace std;
 
-bool isLoShuMagicSquare(int arr[][3]) {
-    const int targetSum = 15; // The sum of each row, column, and diagonal in a Lo Shu Magic Square
-    int rowSums[3] = {0, 0, 0};
-    int colSums[3] = {0, 0, 0};
-    int diag1Sum = 0;
-    int diag2Sum = 0;
+const int ROWS = 3;
+const int COLS = 3;
 
-    // Iterate over each element in the array
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            int num = arr[i][j];
-
-            // Check if the number is in the range 1-9
-            if (num < 1 || num > 9) {
-                return false;
-            }
-
-            // Update the row and column sums
-            rowSums[i] += num;
-            colSums[j] += num;
-
-            // Update the diagonal sums
-            if (i == j) {
-                diag1Sum += num;
-            }
-            if (i + j == 2) {
-                diag2Sum += num;
-            }
-        }
+bool is_lo_shu_magic_square(int arr[][COLS])
+{
+    // Tính tổng các phần tử trên đường chéo chính
+    int main_diag_sum = arr[0][0] + arr[1][1] + arr[2][2];
+    // Tính tổng các phần tử trên đường chéo phụ
+    int other_diag_sum = arr[0][2] + arr[1][1] + arr[2][0];
+    // Nếu tổng các phần tử trên đường chéo chính và đường chéo phụ không bằng nhau, mảng không phải là Mã Lục Thư Ma Trận
+    if (main_diag_sum != other_diag_sum) {
+        return false;
     }
-
-    // Check if all the row, column, and diagonal sums are equal to the target sum
-    int sums[8] = {rowSums[0], rowSums[1], rowSums[2], colSums[0], colSums[1], colSums[2], diag1Sum, diag2Sum};
-    for (int i = 0; i < 8; i++) {
-        if (sums[i] != targetSum) {
+    // Tính tổng các phần tử trên mỗi hàng
+    int row_sum = 0;
+    for (int i = 0; i < ROWS; i++) {
+        int curr_row_sum = 0;
+        for (int j = 0; j < COLS; j++) {
+            curr_row_sum += arr[i][j];
+        }
+        // Nếu tổng các phần tử trên một hàng khác với tổng các phần tử trên đường chéo chính và đường chéo phụ, mảng không phải là Mã Lục Thư Ma Trận
+        if (curr_row_sum != main_diag_sum) {
             return false;
         }
     }
+    // Tính tổng các phần tử trên mỗi cột
+    int col_sum = 0;
+    for (int j = 0; j < COLS; j++) {
+        int curr_col_sum = 0;
+        for (int i = 0; i < ROWS; i++) {
+            curr_col_sum += arr[i][j];
+        }
+        // Nếu tổng các phần tử trên một cột khác với tổng các phần tử trên đường chéo chính và đường chéo phụ, mảng không phải là Mã Lục Thư Ma Trận
+        if (curr_col_sum != main_diag_sum) {
+            return false;
+        }
+    }
+    // Nếu mảng thỏa mãn các điều kiện trên, mảng là Mã Lục Thư Ma Trận
     return true;
 }
+
 int main()
 {
-   int arr[3][3] = {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
-cout << boolalpha << isLoShuMagicSquare(arr) << endl; // true
-
-int arr2[3][3] = {{4, 9, 2}, {3, 5, 8}, {8, 1, 6}};
-cout << boolalpha << isLoShuMagicSquare(arr2) << endl; // false
-
+    // Khởi tạo mảng
+    int arr[ROWS][COLS] = { {4, 9, 2}, {3, 5, 7}, {8, 1, 6} };
+    // Kiểm tra xem mảng có phải là Mã Lục Thư Ma Trận hay không
+    if (is_lo_shu_magic_square(arr)) {
+        cout << "Day la Ma Luc Thu Ma Tran\n";
+    } else {
+        cout << "Day khong phai la Ma Luc Thu Ma Tran\n";
+    }
+    return 0;
 }
